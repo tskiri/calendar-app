fb0c2e37614a798331dfc1b99d7f3fbbec3df02b
tskiri <t.kiribayashi@infomart.co.jp>
2026-06-05 11:17:03 +0900

    a

--- src/main/java/in/tech_camp/calendar_app/controller/PlanController.java
+++ src/main/java/in/tech_camp/calendar_app/controller/PlanController.java
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.tech_camp.calendar_app.entity.PlanEntity;
-import in.tech_camp.calendar_app.repository.PlanRepository;
 import in.tech_camp.calendar_app.service.PlanService;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
